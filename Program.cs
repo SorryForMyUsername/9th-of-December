a5492eda78d8d862a39eb0f830593dcf25067ef1
Ilya Shichkin <ilyashichin29@gmail.com>
2024-12-09 20:23:28 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Практикум_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] omas = { 1, 2, 3, 0, 4, -2 };
+            Console.WriteLine($"Размер: {omas.Length}");
+
+            string[] array = { "Hello", "world", "!" };
+            int length = array.Length;
+            Console.WriteLine($"Размер: {length}");
+        }
+    }
+}
