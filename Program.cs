226c585e6587a42c47cc5e4b3119980800a00ba3
Ilya Shichkin <ilyashichin29@gmail.com>
2024-12-09 20:47:12 +0200

    Задача 2

--- Program.cs
+++ Program.cs
@@ -7,18 +7,49 @@ using System.Threading.Tasks;
 namespace Практикум_11
 {
     /// <summary>
-    /// Задача 1. Определить количество элементов одномерного массива.
+    /// Задача 2. Обнулить заданный диапазон элементов в одномерном массиве.
     /// </summary>
     internal class Program
     {
+        static int Size()
+        {
+            Console.Write("Размер о.м.: ");
+            int size;
+            while(!int.TryParse(Console.ReadLine(), out size) || size <= 0)
+            {
+                Console.WriteLine("Ошибка");
+            }
+            return size;
+        }
+
+        static int[] Input(int length, string name)
+        {
+            int[] array = new int[length];
+
+            for(int i = 0; i < length; i++)
+            {
+                Console.Write($"{name}[{i}] = ");
+                array[i] = int.Parse(Console.ReadLine());
+            }
+
+            return array;
+        }
+
+        static void OutputArray(int[] array, string name)
+        {
+            Console.WriteLine($"Массив {name}:");
+
+            for(int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine($"{name}[{i}] = {array[i]}");
+            }
+        }
+
         static void Main(string[] args)
         {
-            int[] omas = { 1, 2, 3, 0, 4, -2 };
-            Console.WriteLine($"Размер: {omas.Length}");
-
-            string[] array = { "Hello", "world", "!" };
-            int length = array.Length;
-            Console.WriteLine($"Размер: {length}");
+            int[] omas = Input(Size(), "omas");
+            Array.Clear(omas, 1, 2);
+            OutputArray(omas, "omas");
         }
     }
 }
