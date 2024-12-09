270d1aaa35c59752ca8e377f7cdc631897d2a056
Ilya Shichkin <ilyashichin29@gmail.com>
2024-12-09 20:52:18 +0200

    Задача 3

--- Program.cs
+++ Program.cs
@@ -7,49 +7,16 @@ using System.Threading.Tasks;
 namespace Практикум_11
 {
     /// <summary>
-    /// Задача 2. Обнулить заданный диапазон элементов в одномерном массиве.
+    /// Задача 3. Найти максимальное и минимальное значение в массиве.
     /// </summary>
     internal class Program
     {
-        static int Size()
-        {
-            Console.Write("Размер о.м.: ");
-            int size;
-            while(!int.TryParse(Console.ReadLine(), out size) || size <= 0)
-            {
-                Console.WriteLine("Ошибка");
-            }
-            return size;
-        }
-
-        static int[] Input(int length, string name)
-        {
-            int[] array = new int[length];
-
-            for(int i = 0; i < length; i++)
-            {
-                Console.Write($"{name}[{i}] = ");
-                array[i] = int.Parse(Console.ReadLine());
-            }
-
-            return array;
-        }
-
-        static void OutputArray(int[] array, string name)
-        {
-            Console.WriteLine($"Массив {name}:");
-
-            for(int i = 0; i < array.Length; i++)
-            {
-                Console.WriteLine($"{name}[{i}] = {array[i]}");
-            }
-        }
-
         static void Main(string[] args)
         {
-            int[] omas = Input(Size(), "omas");
-            Array.Clear(omas, 1, 2);
-            OutputArray(omas, "omas");
+            int[] omas = { 3, 8, -24, 50, 0, 2 };
+            int max = omas.Max();
+            int min = omas.Min();
+            Console.WriteLine($"Max: {max}\nMin: {min}");
         }
     }
 }
