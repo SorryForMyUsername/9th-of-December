b860c13d43ab4bfdd7653950041294e50e6cf9cc
Ilya Shichkin <ilyashichin29@gmail.com>
2024-12-09 22:14:12 +0200

    Задача 10

--- Program.cs
+++ Program.cs
@@ -7,15 +7,42 @@ using System.Threading.Tasks;
 namespace Практикум_11
 {
     /// <summary>
-    /// Задача 9. Поиск индекса элемента.
+    /// Задача 10. Удаление всех четных элементов из массива.
     /// </summary>
     internal class Program
     {
+        static int[] RandomArray(int length, int min, int max)
+        {
+            int[] array = new int[length];
+            Random rnd = new Random();
+
+            for (int i = 0; i < length; i++)
+            {
+                array[i] = rnd.Next(min, max);
+            }
+
+            return array;
+        }
+
+        static void OutputArray(int[] array, string name)
+        {
+            Console.WriteLine($"Массив {name}:");
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine($"{name}[{i}] = {array[i]}");
+            }
+        }
+
         static void Main(string[] args)
         {
-            int[] omas = { 3, 8, -24, 50, 0, 2 };
-            int index = Array.IndexOf(omas, 50);
-            Console.WriteLine($"Индекс элемента 50 массива omas: {index}");
+            int[] omas = RandomArray(10, 0, 100);
+            OutputArray(omas, "omas");
+
+            Console.WriteLine("\nУдаление чётных элементов массива.\n");
+
+            omas = Array.FindAll(omas, el => el % 2 != 0);
+            OutputArray(omas, "omas");
 
             Console.ReadKey(true);
         }
