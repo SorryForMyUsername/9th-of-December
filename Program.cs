e0e854619265dbb01aa0cdcba1d62c00c038e4f9
Ilya Shichkin <ilyashichin29@gmail.com>
2024-12-09 20:57:00 +0200

    Задача 4

--- Program.cs
+++ Program.cs
@@ -7,16 +7,29 @@ using System.Threading.Tasks;
 namespace Практикум_11
 {
     /// <summary>
-    /// Задача 3. Найти максимальное и минимальное значение в массиве.
+    /// Задача 4. Сортировка массива по возрастанию.
     /// </summary>
     internal class Program
     {
+        static void OutputArray(string[] array, string name)
+        {
+            Console.WriteLine($"Массив {name}:");
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine($"{name}[{i}] = {array[i]}");
+            }
+        }
+
         static void Main(string[] args)
         {
-            int[] omas = { 3, 8, -24, 50, 0, 2 };
-            int max = omas.Max();
-            int min = omas.Min();
-            Console.WriteLine($"Max: {max}\nMin: {min}");
+            string[] fruits = { "яблоко", "груша", "апельсин", "мандарин" };
+            OutputArray(fruits, "fruits");
+
+            Array.Sort(fruits);
+            OutputArray(fruits, "fruits");
+
+            Console.ReadKey();
         }
     }
 }
