82b55b7f01a65e91ab0ed41613b53e10ad2fea59
Ilya <119899583+SorryForMyUsername@users.noreply.github.com>
2024-12-10 11:56:39 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -16,9 +16,37 @@ namespace Практикум_11
     /// </summary>
     internal class Program
     {
+        static void OutputArray(string[] array, string name)
+        {
+            Console.WriteLine($"Массив {name}:");
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine($"{name}[{i}] = {array[i]}");
+            }
+        }
+
+        static void Output(string output)
+        {
+            Console.WriteLine(output);
+        }
+
         static void Main(string[] args)
         {
             double[] dollarExchangeRate = { 70.3375, 70.3375, 70.3375, 70.3375, 70.3375, 70.3375, 70.3375, 70.3375, 70.3375, 70.3002, 69.6094, 69.0202, 67.7775, 67.5744, 67.5744, 67.5744, 68.2892, 68.6644, 68.8728, 68.8467, 68.6656, 68.6656, 68.6656, 68.6288, 68.7529, 68.9573, 69.1263, 69.3372, 69.3372, 69.3372, 69.5927 };
+            
+            double average = dollarExchangeRate.Average();
+            Output($"Среднее значение КВ: {average}");
+
+            double max = dollarExchangeRate.Max();
+            Output($"Маскимальное значение КВ: {max}");
+
+            double min = dollarExchangeRate.Min();
+            Output($"Минимальное значение КВ: {min}");
+
+            //int dayWithMax
+
+            Console.ReadKey();
         }
     }
 }
